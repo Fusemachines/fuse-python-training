a306cf3babb61916e3a97e1660ec6f26ec84bc50
premanuj <mesubedianuj@gmail.com>
2019-11-24 22:03:25 +0545

    integrate schemas and services with controllers

--- apis/resources/users.py
+++ apis/resources/users.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, current_app, request
 from apis.models.users import User
 from apis.services import UserService
+from apis.schemas import UserSchema
+from collections import defaultdict
 
 user = Blueprint("user", __name__, url_prefix="/users")
 
@@ -8,8 +10,16 @@ user_service = UserService()
 
 @user.route("/", methods=["POST"])
 def users():
-    data = request.json
-    user = user_service.create(data)
+    data = UserSchema()
+    errors = data.validate(request.json)
+    if errors:
+        error_list = defaultdict(list)
+        for field, value in errors.items():
+            error_list[field]=value[0]
+        response = dict({"errors": error_list})
+        return jsonify(response), 422
+    valid_data = data.dump(request.json)
+    user = user_service.create(valid_data)
     return jsonify(user)
 
 @user.route("/", methods=["GET"])
