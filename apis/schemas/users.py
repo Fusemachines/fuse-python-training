02456b0aaac9a14d612a66e155d80f3713441eac
premanuj <mesubedianuj@gmail.com>
2019-11-24 21:40:44 +0545

    add schema for user

--- apis/schemas/users.py
+++ apis/schemas/users.py
@@ -1,4 +1,8 @@
+from apis.models import User
 from flask_marshmallow.schema import Schema
+from flask_marshmallow.fields import fields
 
-
-class UserSchame(Schema):
+class UserSchema(Schema):
+    email = fields.Email(required=True)
+    name = fields.String()
+    address = fields.String(required=True)
